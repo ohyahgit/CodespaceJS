32ac64b055f2dd3a223b76e394a2a8ccfbf222a1
ohyahgit <39706781+ohyahgit@users.noreply.github.com>
2023-05-23 14:47:24 +0000

    Add try catch logic to index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import chalk from 'chalk'
 
 console.log(chalk.bold.greenBright('OK!'));
+try {
+    tatltuae = 42;
+    me = "hello";
+    tatltuae = 'hi';
+} catch (Error) {
+    console.log(chalk.bold.redBright('Error'));
+    console.log(chalk.bold.red('You cannot make a number a string!'));
+}
